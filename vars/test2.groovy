2ab7327d1235c62c3e3d0e7439def58373525edc
manupanand <manupanandindianinvestor@gmail.com>
2025-08-16 11:05:01 +0530

    updated code

--- vars/test2.groovy
+++ vars/test2.groovy
@@ -2,5 +2,6 @@ def call() {
     node {
         echo "DEBUG: currentBuild = ${currentBuild}"
         echo "DEBUG: env.BRANCH_NAME = ${env.BRANCH_NAME}"
+        echo "DEBUG: env.appType=${env.appType}"
     }
 }
