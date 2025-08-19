52e276541a70da7371ad72d719523620ed23e058
manupanand <manupanandindianinvestor@gmail.com>
2025-08-19 12:30:14 +0530

    updated firebase pipleine

--- vars/firebase_pipleine.groovy
+++ vars/firebase_pipleine.groovy
@@ -4,6 +4,13 @@ def call(){
         def user   = currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause')[0]?.getUserId()
         def app    = env.appType
         if(app == "nodejs"){
+            stage('Checkout') {
+                checkout scm
+                echo "Branch: ${branch}"
+                echo "Triggered by: ${user}"
+                echo "trigeged okay"
+                
+            }
             
         }   
     }
